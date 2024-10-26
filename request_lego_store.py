267071c8496d728e4e5477a7fcd0fe6f7a0061e9
Adam Taylor <afranklint16@gmail.com>
2024-10-25 23:44:12 -0700

    The API for the lego store is finished!
    Co-authored-by: roboticsdude60 <composerofsoftware@gmail.com>

--- request_lego_store.py
+++ request_lego_store.py
@@ -1,34 +1,54 @@
 import sys
-import json
+import logging
 import requests
 
-
-def get_lego_store_result_for_element_id(element_id):
-    return {}
-
-
-def make_graphql_request(json_body, headers):
-    """
-    Make a GraphQL request and return the response JSON.
-
-    Args:
-        query (str): The GraphQL query string.
-        headers (dict): The request headers.
-
-    Returns:
-        dict: The response JSON object.
-    """
-    url = "https://www.lego.com/api/graphql/PickABrickQuery"  # Replace with the actual GraphQL endpoint
-    response = requests.post(url, json=json_body, headers=headers)
-    response.raise_for_status()  # Raise an exception for HTTP errors
-    return response.json()
-
+QUERY = '''
+query PickABrickQuery($input: ElementQueryInput!) {
+  searchElements(input: $input) {
+    results {
+      ...ElementLeaf
+    }
+    total
+    count
+  }
+}
+ fragment ElementLeaf on SearchResultElement {
+  id
+  maxOrderQuantity
+  deliveryChannel
+  price {
+    centAmount
+    currencyCode
+  }
+}
+'''
 
 def parse_json(response_json):
     results = response_json['data']['searchElements']['results']
     if len(results) > 0:
         return results
     raise results
+
+
+def get_lego_store_result_for_element_id(element_id):
+    url = "https://www.lego.com/api/graphql/PickABrickQuery" 
+    json_body = {
+        "operationName": "PickABrickQuery",
+        "variables": {"input": {"perPage": 10, "query": str(element_id)}},
+        "query": QUERY
+    }
+    headers = {}
+
+    response = requests.post(url, json=json_body, headers=headers)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    response_json = response.json()
+
+    results = parse_json(response_json)
+    if len(results) < 1:
+        logging.error(f"Did not receive result for element ID: {element_id}")
+        return
+    print(f"Received {len(results)} results for element ID: {element_id}, returning the first one.")
+    return results[0]
 
 
 def main():
@@ -41,53 +61,7 @@ def main():
 
     element_id = int(sys.argv[1])
 
-    query = '''
-query PickABrickQuery($input: ElementQueryInput!, $sku: String) {
-  searchElements(input: $input) {
-    results {
-      ...ElementLeaf
-       }
-     total
-    count
-     }
-}
-
- fragment ElementLeaf on SearchResultElement {
-  id
-  maxOrderQuantity
-  name
-  deliveryChannel
-  price {
-    centAmount
-    currencyCode
-  }
-  quantityInSet(sku: $sku)
-  facets {
-    color {  ...ElementFacetCategory}
-  }
-}
-
-fragment ElementFacetCategory on ElementCategory {
-  name
-  key
-}
-'''
-
-    json_body = {
-        "operationName": "PickABrickQuery",
-        "variables": {"input": {"perPage": 10, "query": str(element_id)}},
-        "query": query,
-    }
-
-    # Define the request headers
-    headers = {}
-
-    try:
-        response_json = make_graphql_request(json_body, headers)
-        parse_json(response_json)
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        sys.exit(1)
+    print(get_lego_store_result_for_element_id(element_id))
 
 
 if __name__ == "__main__":
