b2f99854cae1ddb20c175512856473741f0805f5
Adam Taylor <afranklint16@gmail.com>
2024-10-26 16:40:28 -0700

    Cleanup

--- convert.py
+++ convert.py
@@ -22,14 +22,6 @@ from request_bricklink import get_color_dict_for_part
 from request_lego_store import get_lego_store_result_for_element_id
 
 
-RESOLVE_METHODS = {
-    'A': 'Choose best-seller',
-    'B': 'Choose non-best-seller',
-    'C': 'Choose whichever has larger lot count',
-    'D': 'Choose whichever has larger total part count',
-}
-
-
 def split_array_into_equal_chunks(array, num_chunks):
     """
     Break an array into num_chunks roughly equal pieces.
@@ -91,13 +83,12 @@ def main():
     parser.add_argument('-db', '--database_file', default='part_info.db', help='Path to the SQLite database file')
     parser.add_argument('-pb', '--purge_bricklink', action='store_true', help='Purge the BrickLink table in the database before processing the XML')
     parser.add_argument('-pl', '--purge_lego_store', action='store_true', help='Purge the LEGO Pick-a-Brick table in the database before processing the XML')
-    parser.add_argument('-r', '--resolve_method', default=None, choices=RESOLVE_METHODS.keys(), help='Method to resolve duplicate store options for a part (bestseller vs non-bestseller)')
     args = parser.parse_args()
 
     logger = setup_logger(args.log_file)
-    
+
     database = DatabaseManager(args.database_file, logger)
-    
+
     # Don't actually convert if purge is requested
     if args.purge_bricklink:
         database.purge_bricklink_table()
@@ -105,19 +96,19 @@ def main():
     if args.purge_lego_store:
         database.purge_lego_store_table()
         return
-    
+
     # Step 0 - Parse input XML file
     bricklink_xml_partslist = parse_xml(args.input_xml)
     logging.info(f"Step 0 complete - bricklink XML partslist (length {len(bricklink_xml_partslist)}): {bricklink_xml_partslist}")
-    
+
     # Step 1 - round up all design IDs
     unique_design_ids = {part['design_id'] for part in bricklink_xml_partslist}
     logging.info(f"Step 1 complete - unique design IDs (length {len(unique_design_ids)}): {unique_design_ids}")
-    
+
     # Step 2 - Find out which design IDs are NOT in the bricklink database table
     request_design_ids = {design_id for design_id in unique_design_ids if not database.get_bricklink_entry_by_design_id(design_id)}
     logging.info(f"Step 2 complete - request design IDs (length {len(request_design_ids)}): {request_design_ids}")
-    
+
     # Step 3 - Make the requests to bricklink for all the missing design IDs
     start_time = time.time()
     database_insertions = 0
@@ -137,18 +128,18 @@ def main():
     elapsed_time = end_time - start_time
     minutes, seconds = divmod(int(elapsed_time), 60)
     logging.info(f"Step 3 complete - database insertions: {database_insertions}, time taken: {minutes} minutes and {seconds} seconds")
-    
+
     # Step 4 - Create a master list of all potential element IDs
     master_element_ids = set()
     for part in bricklink_xml_partslist:
         element_ids = [element_id for (element_id, _, _) in database.get_bricklink_entries_by_design_id_and_color_code(part['design_id'], part['color_id'])]
         master_element_ids.update(element_ids)
     logging.info(f"Step 4 complete - master element IDs (length {len(master_element_ids)}): {master_element_ids}")
-    
+
     # Step 5 - Find out which element IDs are not in the lego pick-a-brick database table
     request_element_ids = {element_id for element_id in master_element_ids if not database.get_lego_store_entry_by_element_id(element_id)}
     logging.info(f"Step 5 complete - request element IDs (length {len(request_element_ids)}): {request_element_ids}")
-    
+
     # Step 6 - Make the requests to lego pick-a-brick for all the missing element IDs
     start_time = time.time()
     database_insertions = 0
@@ -164,7 +155,11 @@ def main():
                 else:
                     if data['deliveryChannel'] not in ['pab', 'bap']:
                         raise ValueError(f"Invalid delivery channel: {data['deliveryChannel']}")
-                    database.insert_lego_store_entry(element_id, lego_sells=True, bestseller=(data['deliveryChannel'] == 'pab'), price=data['price']['centAmount'], max_order_quantity=data['maxOrderQuantity'])
+                    database.insert_lego_store_entry(element_id,
+                                                     lego_sells=True,
+                                                     bestseller=(data['deliveryChannel'] == 'pab'),
+                                                     price=data['price']['centAmount'],
+                                                     max_order_quantity=data['maxOrderQuantity'])
                     database_insertions += 1
             except Exception as exc:
                 logging.error(f"Step 6 - Element ID {element_id} generated an exception: {exc}")
@@ -172,27 +167,16 @@ def main():
     elapsed_time = end_time - start_time
     minutes, seconds = divmod(int(elapsed_time), 60)
     logging.info(f"Step 6 complete - database insertions: {database_insertions}, time taken: {minutes} minutes and {seconds} seconds")
-    
+
     # Step 7 - Resolve all potential issues with the data
-    #   case 1 = part doesn't exist
-    #   case 2 = part exists, one version
-    #   case 3 = part exists, multiple versions (likely 1 each for bestseller & 1 not)
-    # DECIDE - What do about parts in each of these cases?
-    # DECIDE - how to split up if reached maximum part count?
-    # FACT: 200 max lots for bestseller cart, $7 service fee for orders under $14,shipping $11.95
-    # FACT: 200 max lots for non-bestseller cart, $7 service fee for orders under $14, shipping $11.95
-    # FACT: If shipping both bestseller and non-bestseller, $18.95 shipping for both
-    # FACT: max quantity of a single item is 999
-    # FACT: If you have a large enough order, free shipping
-    
+
     not_available_final_list = []
     not_available_total_count = 0
     bucket_one_available = []
     bucket_two_available = []
-    
+
     # Step 7.1 - For each part in the original list, find out how many elements it has that LEGO sells
     LEGO_SELLS_COLUMN = 4
-
     for part in bricklink_xml_partslist:
         available_elements = 0
         element_results = database.match_bricklink_entries_to_lego_store_entries(part['design_id'], part['color_id'])
@@ -210,17 +194,17 @@ def main():
             bucket_two_available.append(part)
         else:
             logging.error(f"Part {part} has {available_elements} available elements from the store")
-    
+
     logging.info(f"Step 7.1 complete - bucket not available (length {len(not_available_final_list)}): {not_available_final_list}")
     logging.info(f"Step 7.1 complete - bucket one available (length {len(bucket_one_available)}): {bucket_one_available}")
     logging.info(f"Step 7.1 complete - bucket two available (length {len(bucket_two_available)}): {bucket_two_available}")
-    
+
     # Step 7.2 - Find out how many "one available" options are bestseller vs not
     BESTSELLER_COLUMN = 5
 
     bestseller_final_list = []
     bestseller_total_count = 0
-    
+
     non_bestseller_final_list = []
     non_bestseller_total_count = 0
 
@@ -236,17 +220,19 @@ def main():
                     non_bestseller_final_list.append({'elementId': element_result[0], 'quantity': part['quantity']})
                     non_bestseller_total_count += int(part['quantity'])
 
-    logging.info(f"Step 7.2 complete - In the 'one available' bucket, bestseller has {len(bestseller_final_list)} lots and {bestseller_total_count} total parts, non-bestseller has {len(non_bestseller_final_list)} lots and {non_bestseller_total_count} total parts")
-    
+    logging.info(f"Step 7.2 complete - In the 'one available' bucket, bestseller has {len(bestseller_final_list)} lots \
+                   and {bestseller_total_count} total parts, non-bestseller has {len(non_bestseller_final_list)} lots \
+                   and {non_bestseller_total_count} total parts")
+
     # Step 7.3 - Compare prices and max order quantity for each of the "two available" parts
     PRICE_COLUMN = 6
     MAX_ORDER_QUANTITY_COLUMN = 7
-    
+
     for part in bucket_two_available:
         element_results = database.match_bricklink_entries_to_lego_store_entries(part['design_id'], part['color_id'])
-        
+
         options = []
-        
+
         for element_result in element_results:
             if element_result[LEGO_SELLS_COLUMN]:
                 options.append({
@@ -256,8 +242,9 @@ def main():
                     'maxOrderQuantity': element_result[MAX_ORDER_QUANTITY_COLUMN],
                     'bestseller': element_result[BESTSELLER_COLUMN]
                 })
-                logger.info(f"Comparing part {part} with element ID {element_result[0]} - Max Order Quantity: {element_result[MAX_ORDER_QUANTITY_COLUMN]}, BestSeller = {element_result[BESTSELLER_COLUMN]}, Price: {element_result[PRICE_COLUMN]} cents")
-            
+                logger.info(f"Comparing part {part} with element ID {element_result[0]} - Max Order Quantity: {element_result[MAX_ORDER_QUANTITY_COLUMN]}, \
+                              BestSeller = {element_result[BESTSELLER_COLUMN]}, Price: {element_result[PRICE_COLUMN]} cents")
+
         print("You must choose one of the following options:")
         for i, option in enumerate(options):
             print(f"{i+1}. {option}")
@@ -283,15 +270,17 @@ def main():
             non_bestseller_total_count += int(part['quantity'])
 
         logging.info(f"User chose option {option}")
-    
-    logger.info(f"Step 7.3 complete - Price and max order quantity comparison complete; bestseller now has {len(bestseller_final_list)} lots and {bestseller_total_count} total parts, non-bestseller has {len(non_bestseller_final_list)} lots and {non_bestseller_total_count} total parts")
-            
+
+    logger.info(f"Step 7.3 complete - Price and max order quantity comparison complete; bestseller now has {len(bestseller_final_list)} lots \
+                  and {bestseller_total_count} total parts, non-bestseller has {len(non_bestseller_final_list)} lots \
+                  and {non_bestseller_total_count} total parts")
+
     # Step 8 - export the data to the output file
-    
+
     # Step 8.1 - export the not available parts
     not_available_filename = os.path.join(os.path.dirname(args.input_xml), os.path.splitext(os.path.basename(args.input_xml))[0] + '_not_available.xml')
     export_xml(not_available_final_list, not_available_filename)
-    
+
     # Step 8.2 - Break lists into sub-lists to keep each under 200 lots
     bestseller_chunks = []
     num_bestseller_chunks = 0
@@ -310,9 +299,10 @@ def main():
     else:
         num_non_bestseller_chunks = 1
         non_bestseller_chunks = [non_bestseller_final_list]
-        
-    logger.info(f"Step 8.2 complete - bestseller list will be written in {num_bestseller_chunks} chunks, non-bestseller list will be written in {num_non_bestseller_chunks} chunks")
-    
+
+    logger.info(f"Step 8.2 complete - bestseller list will be written in {num_bestseller_chunks} chunks, non-bestseller list \
+                  will be written in {num_non_bestseller_chunks} chunks")
+
     # Step 8.3 - export all parts to CSV or JSON, breaking up the individual lists to be under 200 lots per delivery
     export_function = None
     export_extension = None
@@ -324,10 +314,11 @@ def main():
         export_extension = 'json'
     else:
         logging.error(f"Invalid output file type: {args.output_file}")
-        
+
     if export_function is not None:
         for i in range(0, max(num_bestseller_chunks, num_non_bestseller_chunks)):
-            output_filename = os.path.join(os.path.dirname(args.output_file), os.path.splitext(os.path.basename(args.output_file))[0] + f'_order{i+1}.' + export_extension)
+            output_filename = os.path.join(os.path.dirname(args.output_file),
+                                           os.path.splitext(os.path.basename(args.output_file))[0] + f'_order{i+1}.' + export_extension)
             bestseller_chunk = []
             non_bestseller_chunk = []
             if i < num_bestseller_chunks:
@@ -335,10 +326,12 @@ def main():
             if i < num_non_bestseller_chunks:
                 non_bestseller_chunk = non_bestseller_chunks[i]
             export_function(bestseller_chunk + non_bestseller_chunk, output_filename)
-            logger.info(f"Wrote {len(bestseller_chunk)} + {len(non_bestseller_chunk)} = {len(bestseller_chunk + non_bestseller_chunk)} entries to {output_filename}")
-    
+            logger.info(f"Wrote {len(bestseller_chunk)} + {len(non_bestseller_chunk)} = {len(bestseller_chunk + non_bestseller_chunk)} \
+                          entries to {output_filename}")
+
     # Step 9 - close the database
     database.close()
+
 
 if __name__ == '__main__':
     main()
