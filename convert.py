2ef10d836173ec57b6dae013e2deca484969f87f
Adam Taylor <afranklint16@gmail.com>
2024-10-26 11:14:43 -0700

    Adjust print statements and remove step 7 start

--- convert.py
+++ convert.py
@@ -79,17 +79,18 @@ def main():
     
     # Step 0 - Parse input XML file
     bricklink_xml_partslist = parse_xml(args.input_xml)
-    print(f"Step 0 - bricklink xml partslist: {bricklink_xml_partslist}")
+    logging.info(f"Step 0 complete - bricklink XML partslist: {bricklink_xml_partslist}")
     
     # Step 1 - round up all design IDs
     unique_design_ids = {part['design_id'] for part in bricklink_xml_partslist}
-    print(f"Step 1 - unique design IDs: {unique_design_ids}")
+    logging.info(f"Step 1 complete - unique design IDs: {unique_design_ids}")
     
     # Step 2 - Find out which design IDs are NOT in the bricklink database table
     request_design_ids = {design_id for design_id in unique_design_ids if not database.get_bricklink_entry_by_design_id(design_id)}
-    print(f"Step 2 - request design IDs: {request_design_ids}")
+    logging.info(f"Step 2 complete - request design IDs: {request_design_ids}")
     
     # Step 3 - Make the requests to bricklink for all the missing design IDs
+    database_insertions = 0
     with concurrent.futures.ThreadPoolExecutor() as executor:
         future_to_design_id = {executor.submit(get_color_dict_for_part, design_id): design_id for design_id in request_design_ids}
         for future in concurrent.futures.as_completed(future_to_design_id):
@@ -99,21 +100,24 @@ def main():
                 for color_code, element_id_list in data.items():
                     for element_id in element_id_list:
                         database.insert_bricklink_entry(element_id, design_id, color_code)
+                        database_insertions += 1
             except Exception as exc:
-                print(f"Step 3 - Design ID {design_id} generated an exception: {exc}")
+                logging.error(f"Step 3 - Design ID {design_id} generated an exception: {exc}")
+    logging.info(f"Step 3 complete - database insertions: {database_insertions}")
     
     # Step 4 - Create a master list of all potential element IDs
     master_element_ids = set()
     for part in bricklink_xml_partslist:
         element_ids = [element_id for (element_id, _, _) in database.get_bricklink_entries_by_design_id_and_color_code(part['design_id'], part['color_id'])]
         master_element_ids.update(element_ids)
-        print(f"Step 4 - element IDs for design ID {part['design_id']} and color code {part['color_id']}: {element_ids}")
+    logging.info(f"Step 4 complete - master element IDs: {master_element_ids}")
     
     # Step 5 - Find out which element IDs are not in the lego pick-a-brick database table
     request_element_ids = {element_id for element_id in master_element_ids if not database.get_lego_store_entry_by_element_id(element_id)}
-    print(f"Step 5 - request element IDs: {request_element_ids}")
+    logging.info(f"Step 5 complete - request element IDs: {request_element_ids}")
     
     # Step 6 - Make the requests to lego pick-a-brick for all the missing element IDs
+    database_insertions = 0
     with concurrent.futures.ThreadPoolExecutor() as executor:
         future_to_element_id = {executor.submit(get_lego_store_result_for_element_id, element_id): element_id for element_id in request_element_ids}
         for future in concurrent.futures.as_completed(future_to_element_id):
@@ -122,13 +126,15 @@ def main():
                 data = future.result()
                 if data is None:
                     database.insert_lego_store_entry(element_id, lego_sells=False, bestseller=None, price=None, max_order_quantity=None)
+                    database_insertions += 1
                 else:
                     if data['deliveryChannel'] not in ['pab', 'bap']:
                         raise ValueError(f"Invalid delivery channel: {data['deliveryChannel']}")
                     database.insert_lego_store_entry(element_id, lego_sells=True, bestseller=(data['deliveryChannel'] == 'pab'), price=data['price']['centAmount'], max_order_quantity=data['maxOrderQuantity'])
-                print(f"Step 6 - Element ID {element_id} data: {data}")
+                    database_insertions += 1
             except Exception as exc:
-                print(f"Step 6 - Element ID {element_id} generated an exception: {exc}")
+                logging.error(f"Step 6 - Element ID {element_id} generated an exception: {exc}")
+    logging.info(f"Step 6 complete - database insertions: {database_insertions}")
     
     # Step 7 - Resolve all potential issues with the data
     #   case 1 = part doesn't exist
@@ -141,17 +147,6 @@ def main():
     # FACT: If shipping both bestseller and non-bestseller, $18.95 shipping for both
     # FACT: max quantity of a single item is 999
     # FACT: If you have a large enough order, free shipping
-    bucket_zero = []
-    bucket_one = []
-    bucket_two = []
-    for index, part in enumerate(bricklink_xml_partslist):
-        element_ids = [element_id for (element_id, _, _) in database.get_bricklink_entries_by_design_id_and_color_code(part['design_id'], part['color_id'])]
-        for element_id in element_ids:
-            lego_store_entry = database.get_lego_store_entry_by_element_id(element_id)
-            if lego_store_entry is None:
-                print(f"Step 7 - Element ID {element_id} does not exist in the LEGO Pick-a-Brick database")
-            else:
-                print(f"Step 7 - Element ID {element_id} data: {lego_store_entry}")
     
     # Step 8 - export the data to the output file
     
