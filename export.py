25090c2760921d69669e661fb99eb4e861bd7c98
Adam Taylor <afranklint16@gmail.com>
2024-10-25 18:07:51 -0700

    Conform to style

--- export.py
+++ export.py
@@ -1,10 +1,21 @@
+"""Export.
+
+This module provides functions to export partslist data to CSV and JSON files.
+"""
+
 import csv
 import json
 import logging
 
 
-# Save partslist data to CSV file
 def export_csv(data, path):
+    """
+    Save partslist data to a CSV file.
+
+    Args:
+        data (list of dict): The partslist data to be exported.
+        path (str): The file path where the CSV file will be saved.
+    """
     fields = ['elementId', 'quantity']
     with open(path, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
@@ -13,8 +24,14 @@ def export_csv(data, path):
     logging.info(f"Exported {len(data)} entries to {path}")
 
 
-# Save partslist data to JSON file
 def export_json(data, path):
+    """
+    Save partslist data to a JSON file.
+
+    Args:
+        data (list of dict): The partslist data to be exported.
+        path (str): The file path where the JSON file will be saved.
+    """
     with open(path, 'w') as jsonfile:
         json.dump(data, jsonfile, indent=4)
     logging.info(f"Exported {len(data)} entries to {path}")
